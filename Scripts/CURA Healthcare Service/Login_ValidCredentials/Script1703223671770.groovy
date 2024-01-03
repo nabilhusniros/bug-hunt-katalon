d1132955c848e61d2bc80c416165d25cdd76846c
nabilhusniros <nabilhusnir@gmail.com>
2024-01-03 11:51:11 +0800

    Added Regression Test Suite and Profiles

--- Scripts/CURA Healthcare Service/Login_ValidCredentials/Script1703223671770.groovy
+++ Scripts/CURA Healthcare Service/Login_ValidCredentials/Script1703223671770.groovy
@@ -19,7 +19,7 @@ import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
 
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+WebUI.navigateToUrl(GlobalVariable.URL)
 
 WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/btnMakeAppointment'))
 
